86c6cc844c1465611d4ccdf5111e40b1107fc493
Farooq Kazi <farooq.kazi86@gmail.com>
2019-06-28 20:53:23 +0530

    added different Loading State

--- app/src/main/java/com/mycakes/ui/MainActivity.kt
+++ app/src/main/java/com/mycakes/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.mycakes.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -12,10 +13,11 @@ import com.mycakes.ui.adapter.listener.CakeClickListener
 import com.mycakes.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
     lateinit var viewModel: MainViewModel
     lateinit var cakeAdapter: CakeAdapter
-    val cakeClickListener: CakeClickListener = object : CakeClickListener {
+    private val cakeClickListener: CakeClickListener = object : CakeClickListener {
         override fun onClick(cake: Cake) {
             //Display Cake Description
         }
@@ -28,14 +30,47 @@ class MainActivity : AppCompatActivity() {
         setupRecyclerView()
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         viewModel.cakes.observe(this, Observer {
-            if (it.isNotEmpty()) {
-                cakeAdapter.cakes.clear()
-                cakeAdapter.cakes.addAll(it)
-                cakeAdapter.notifyDataSetChanged()
-            }
+            cakeAdapter.cakes.clear()
+            cakeAdapter.cakes.addAll(it)
+            cakeAdapter.notifyDataSetChanged()
+
         })
 
+        viewModel.errorMessage.observe(this, Observer {
+            tvMessage.text = it
+        })
 
+        viewModel.loadingState.observe(this, Observer {
+            when (it) {
+                MainViewModel.LoadingState.LOADING -> displayProgressbar()
+                MainViewModel.LoadingState.SUCCESS -> displayList()
+                MainViewModel.LoadingState.ERROR -> displayMessageContainer()
+                else-> displayMessageContainer()
+            }
+        })
+        if (viewModel.lastFetchedTime == null) {
+            viewModel.fetchCakes()
+        }
+
+    }
+
+    private fun displayProgressbar() {
+        progressbar.visibility = View.VISIBLE
+        rvCakes.visibility = View.GONE
+        llMessageContainer.visibility = View.GONE
+    }
+
+    private fun displayMessageContainer() {
+        llMessageContainer.visibility = View.VISIBLE
+        rvCakes.visibility = View.GONE
+        progressbar.visibility = View.GONE
+    }
+
+    private fun displayList() {
+
+        llMessageContainer.visibility = View.GONE
+        rvCakes.visibility = View.VISIBLE
+        progressbar.visibility = View.GONE
     }
 
     private fun setupRecyclerView() {
--- app/src/main/java/com/mycakes/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/mycakes/viewmodel/MainViewModel.kt
@@ -1,31 +1,46 @@
 package com.mycakes.viewmodel
 
+import android.os.Handler
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mycakes.data.model.Cake
+import java.util.*
 
 class MainViewModel : ViewModel() {
+    fun fetchCakes() {
+        loadingState.value = LoadingState.LOADING
+        Handler().postDelayed({
+            cakes.value = listOf(
+                Cake(
+                    "Lemon cheesecake", "Lemon cheesecake",
+                    "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"
+                ),
+                Cake(
+                    "Lemon cheesecake", "Lemon cheesecake",
+                    "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"
+                ),
+                Cake(
+                    "Lemon cheesecake", "Lemon cheesecake",
+                    "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"
+                )
+            )
+            loadingState.value = LoadingState.SUCCESS
+        }, 5000)
 
-    val cakes: MutableLiveData<List<Cake>> = MutableLiveData<List<Cake>>().apply {
-        value = listOf(
-            Cake("Lemon cheesecake", "A cheesecake made of lemon", "https://s3-eu-west-1.amazonaws.com/s3.mediafileserver.co.uk/carnation/WebFiles/RecipeImages/lemoncheesecake_lg.jpg"),
-            Cake("Demo 2 ", "Demo 2", "Demo 2"),
-            Cake("Demo", "Demo", "Demo 2"),
-            Cake("Demo 2 ", "Demo 2", "Demo 2"),
-            Cake("Demo", "Demo", "Demo 2"),
-            Cake("Demo 2 ", "Demo 2", "Demo 2"),
-            Cake("Demo", "Demo", "Demo 2"),
-            Cake("Demo 2 ", "Demo 2", "Demo 2"),
-            Cake("Demo", "Demo", "Demo 2"),
-            Cake("Demo 2 ", "Demo 2", "Demo 2")
-        )
     }
 
-    val errorMessage: MutableLiveData<String> = MutableLiveData<String>().apply {
-        value = ""
-    }
+    var lastFetchedTime: Date? = null
 
-    val isLoading: MutableLiveData<Boolean> = MutableLiveData<Boolean>().apply {
-        value = false
+    val cakes: MutableLiveData<List<Cake>> = MutableLiveData()
+
+    val errorMessage: MutableLiveData<String> = MutableLiveData()
+
+
+    val loadingState = MutableLiveData<LoadingState>()
+
+    enum class LoadingState {
+        LOADING,
+        SUCCESS,
+        ERROR
     }
 }
